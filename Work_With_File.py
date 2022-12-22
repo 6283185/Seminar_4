3db525096a7b9d244c1f3b2887cc21d5f5797d99
Dmitry <chernyshov85@mail.ru>
2022-12-22 23:06:32 +0300

    Task_1

--- Work_With_File.py
+++ Work_With_File.py
@@ -1,3 +1,9 @@
-f = open('test.txt', 'w')
-f.write('Hello world!')
-f.close()
+# file_path = r'C:\Users\Home\Desktop\GeekBrains\python\Seminar_4\test.txt'
+# f = open(file_path, 'w')
+# f.write('Hello world!')
+# f.close()
+
+file_path = r'C:\Users\Home\Desktop\GeekBrains\python\Seminar_4\test_2.txt'
+with open(file_path, 'w') as f_data:
+    # print(f_data.read())
+    f_data.write('Hello world!')
