18987611fdb2218038820dfb42e3c7b6573100db
Dmitry <chernyshov85@mail.ru>
2022-12-23 08:14:17 +0300

    HW

--- Task_1.py
+++ Task_1.py
@@ -2,3 +2,18 @@
 #  Напишите программу, которая покажет большее и меньшее число.
 # В качестве символа-разделителя используйте пробел. 
 # Результат заисать в конец исходного файла (х1 х2).
+
+file_path = r'C:\Users\Home\Desktop\GeekBrains\python\Seminar_4\task_1.txt'
+with open(file_path, 'w') as f_data:
+    f_data.write('1 4 5 6 8 2 6 8\n')
+with open(file_path, 'r') as f_data:
+    lst = f_data.read()
+new_lst = lst.split(" ")
+lst_int = [int(i) for i in new_lst]
+max_num = max(lst_int)
+min_num = min(lst_int)
+
+
+with open(file_path, 'a') as f_data:
+    f_data.write(f'Max num is {max_num}\n')
+    f_data.write(f'Min num is {min_num}')
